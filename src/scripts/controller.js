6da46fcebabd7c45f03878197a89b1499db1dc46
Denis Derkach <podstavka128@gmail.com>
2019-02-22 19:03:17 +0200

    feature/CurrencyController, - init controller - use getCurrency service - create setMethod mathod - create exchangeCurrency method - create getExchangeCurrency method

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,5 +1,39 @@
-const app = angular.module('currencyApp', []);
+app.controller('CurrencyController', [
+    'getCurrency',
+    'currencies',
+    'defaultFrom',
+    'defaultTo',
+    'commissionPercantage',
+    'defaultPercantage',
+    function(getCurrency, currencies, defaultFrom, defaultTo, commissionPercantage, defaultPercantage) {
 
-app.controller('CurrencyController', [function() {
-    
+    this.currencies = currencies;
+    this.defaultFrom = defaultFrom;
+    this.defaultTo = defaultTo;
+    this.commissionPercantage = commissionPercantage;
+    this.defaultPercantage = defaultPercantage;
+    this.method = 'buy';
+
+    getCurrency.getData().then(data => {
+        this.data = data;
+    });
+
+    this.setMethod = (method) => {
+        this.method = method;
+        this.exchangeCurrency();
+    };
+
+    this.exchangeCurrency = () => {
+        const rateToBuy = this.data.find(el => el.ccy === this.defaultFrom)[this.method];
+        const rateToSell = this.data.find(el => el.ccy === this.defaultTo)[this.method];
+        const exchangeValue = this.inputToExchange * rateToBuy / rateToSell;
+        this.inputToGet = +exchangeValue.toFixed(2);
+    };
+
+    this.getExchangeCurrency = () => {
+        const rateToBuy = this.data.find(el => el.ccy === this.defaultTo)[this.method];
+        const rateToSell = this.data.find(el => el.ccy === this.defaultFrom)[this.method];
+        const exchangeValue = this.inputToGet * rateToBuy / rateToSell;
+        this.inputToExchange = +exchangeValue.toFixed(2);
+    };
 }]);
