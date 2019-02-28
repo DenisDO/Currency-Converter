758b62f76a449c9d961d5c86244c18ccbb02d22e
Denis Derkach <podstavka128@gmail.com>
2019-02-28 16:32:04 +0200

    feature/currenciesPage: - create component currenciesPage - create template - create controller for currenciesPage

---
+++ src/components/currencies_page/currencies_page.controller.js
@@ -0,0 +1,3 @@
+app.controller('currenciesPageController', [function() {
+
+}]);
---
+++ src/components/currencies_page/index.js
@@ -0,0 +1,5 @@
+app.component('currenciesPage', {
+    bindings: { currencies: '<' },
+    templateUrl: '../templates/about_page/currencies_page.template.html',
+    controller: 'currenciesPageController'
+});
